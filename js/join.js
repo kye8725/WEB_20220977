d57aba3f98b4d6a563f612c878cf0666df9a8d7d
kye <kye8725@naver.com>
2023-06-12 15:25:29 +0000

    2023년 6월 13일 꾸글 홈페이지 최종 수정 완료

--- js/join.js
+++ js/join.js
@@ -1,3 +1,38 @@
+class SignUp {
+  constructor(firstName, lastName, birthdayDate, gender, emailAddress, phoneNumber, classNumber, random) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.birthdayDate = birthdayDate;
+    this.gender = gender;
+    this.emailAddress = emailAddress;
+    this.phoneNumber = phoneNumber;
+    this.classNumber = classNumber;
+    this.random = random;
+  }
+
+  get fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+
+  set fullName(fullName) {
+    const [firstName, lastName] = fullName.split(" ");
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  get contactInfo() {
+    return `${this.emailAddress} ${this.phoneNumber} ${this.random}`;
+  }
+
+  set contactInfo(contactInfo) {
+    const [emailAddress, phoneNumber, random] = contactInfo.split(" ");
+    this.emailAddress = emailAddress;
+    this.phoneNumber = phoneNumber;
+    this.random = random;
+      
+  }
+}
+
 function join(){ // 회원가입
     let form = document.querySelector("#form_main");
     let f_name = document.querySelector("#firstName");
@@ -17,4 +52,39 @@ function join(){ // 회원가입
         session_join_set();
         form.submit();
     }
+}
+
+class SignUp {
+  constructor(firstName, lastName, birthdayDate, gender, emailAddress, phoneNumber, classNumber, random) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+    this.birthdayDate = birthdayDate;
+    this.gender = gender;
+    this.emailAddress = emailAddress;
+    this.phoneNumber = phoneNumber;
+    this.classNumber = classNumber;
+    this.random = random;
+  }
+
+  get fullName() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+
+  set fullName(fullName) {
+    const [firstName, lastName] = fullName.split(" ");
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  get contactInfo() {
+    return `${this.emailAddress} ${this.phoneNumber} ${this.random}`;
+  }
+
+  set contactInfo(contactInfo) {
+    const [emailAddress, phoneNumber, random] = contactInfo.split(" ");
+    this.emailAddress = emailAddress;
+    this.phoneNumber = phoneNumber;
+    this.random = random;
+      
+  }
 }
